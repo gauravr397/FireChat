b164bcb33778507251984f398e17d546d88fa1db
LazyPanda397 <gauravr397@gmail.com>
2022-06-03 00:18:08 +0530

    test_Success -->

--- app/src/main/java/com/example/message/activities/SignInActivity.java
+++ app/src/main/java/com/example/message/activities/SignInActivity.java
@@ -1,17 +1,11 @@
 package com.example.message.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.example.message.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.message.databinding.ActivitySignInBinding;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.remote.FirestoreChannel;
-
-import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
@@ -26,20 +20,6 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
-        binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
     }
-    private void addDataToFirestore(){
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String,Object> data =new HashMap<>();
-        data.put("first_name","gaurav");
-        data.put("last_name", "raghav");
-        database.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(), "Data inserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception -> {
-                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
+
 }
