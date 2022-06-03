e4380a9f35e8f2e2fddd2ed0759495a10406adb2
LazyPanda397 <gauravr397@gmail.com>
2022-06-03 23:46:39 +0530

    ui -->

--- app/src/main/java/com/example/message/activities/SignUpActivity.java
+++ app/src/main/java/com/example/message/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.example.message.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.message.R;
+import com.example.message.databinding.ActivitySignInBinding;
+import com.example.message.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners(){
+            binding.textSignIn.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
