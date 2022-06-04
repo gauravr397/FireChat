4f379c3d77039c7c346fb3eede470aa31b40a3dd
LazyPanda397 <gauravr397@gmail.com>
2022-06-04 23:30:49 +0530

    Constants -->

---
+++ app/src/main/java/com/example/message/utilities/Constants.java
@@ -0,0 +1,11 @@
+package com.example.message.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME ="name";
+    public static final String KEY_EMAIL ="email";
+    public static final String KEY_PSSWORD ="password";
+    public static final String KEY_PREFERENCE_NAME ="chatapppreference";
+    public static final String KEY_USER_ID ="userid";
+    public static final String KEY_IMAGE ="image";
+}
