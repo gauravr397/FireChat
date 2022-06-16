814495ce918a0b25d767ad93482f8f1196106e08
LazyPanda397 <gauravr397@gmail.com>
2022-06-16 22:29:18 +0530

    SignUpActivity -->

--- app/src/main/java/com/example/message/activities/SignInActivity.java
+++ app/src/main/java/com/example/message/activities/SignInActivity.java
@@ -1,7 +1,33 @@
 package com.example.message.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.message.R;
+import com.example.message.databinding.ActivitySignInBinding;
+import com.example.message.databinding.ActivitySignUpBinding;
+import com.example.message.utilities.Constants;
+import com.example.message.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,4 +48,21 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private Boolean isValidSignUpDetails(){
+        if(binding.inputEmail.getText().toString().isEmpty()) {
+            showToast("Enter Email");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter Valid Email");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }else {
+            return true;
+        }
+    }
 }
