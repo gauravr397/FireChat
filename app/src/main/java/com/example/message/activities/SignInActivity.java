9cb6953357b6d01b9726b86e7b77efc7fbc1e523
LazyPanda397 <gauravr397@gmail.com>
2022-06-24 22:40:19 +0530

    SignInActivity -->

--- app/src/main/java/com/example/message/activities/SignInActivity.java
+++ app/src/main/java/com/example/message/activities/SignInActivity.java
@@ -57,22 +57,27 @@ public class SignInActivity extends AppCompatActivity {
     private void signIn(){
         loading(true);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
-        database.collection(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
                 .whereEqualTo(Constants.KEY_PSSWORD, binding.inputPassword.getText().toString())
                 .get()
                 .addOnCompleteListener(task -> {
-                        if{task.isSuccessful() && task.getResult() != null
-                            && task.getResult().getDocuments().size() > 0){
-                            DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
-                            preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
-                            preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
-                            preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
-                            preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
-
-
+                    if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else{
+                        loading(false);
+                        showToast("Unable to Sign in ");
                     }
                         });
     }
+
 
     private void loading(Boolean isLoading){
         if(isLoading){
